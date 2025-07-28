9196f9e225afebe7a3cf182c53a7aac2fb868bb3
Misha <mishadyshkant132@gmail.com>
2025-07-28 18:16:35 +0300

    render v2

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ const TravelPost = require('./models/TravelPost');
 const app = express();
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB error:', err));
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 
